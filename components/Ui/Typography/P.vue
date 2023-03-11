121e9613935fdc16a52dd335ac9e12b5de0166bf
Qaudri <aquadirmuhammad@gmail.com>
2023-03-11 13:39:03 +0100

    refactor: typography components updated to suit project

--- components/Ui/Typography/P.vue
+++ components/Ui/Typography/P.vue
@@ -1,13 +1,5 @@
 <template>
-  <p :class="[is_white ? 'text-white' : 'text-secondary', small_text ? 'text-sm 2xl:text-base' : 'text-sm md:text-base 2xl:text-lg']">
+  <p class="text-sm xl:text-base">
     <slot/>
   </p>
 </template>
-<script>
-  export default {
-    props: {
-      is_white: Boolean,
-      small_text: Boolean
-    }
-  }
-</script>
