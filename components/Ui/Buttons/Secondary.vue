cdfb69633f4abb72fe02b2e87a2824b80b5602b9
Qaudri <aquadirmuhammad@gmail.com>
2023-03-22 17:47:13 +0100

    refactor: secondary and tertiary buttons components renamed to match the contents

--- components/Ui/Buttons/Secondary.vue
+++ components/Ui/Buttons/Secondary.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="emitclicked" class="font-normal text-sm xl:text-base 2xl:text-lg hover:text-primary ease-in-out duration-500 border-b border-transparent hover:border-primary select-none text-left outline-none focus:outline-none focus:border-primary" :class="white_text ? 'text-white' : 'text-accent'">
+    <button @click="emitclicked()" class="py-2 px-5 text-accent border-accent focus:text-white hover:text-white border font-medium text-base 2xl:text-lg justify-center duration-500 ease-in-out rounded-md outline-none focus:outline-none" :class="[flexdisplay ? 'flex w-full' : 'block w-fit', standout ? 'bg-primary  border-primary hover:bg-secondary focus:bg-secondary': 'bg-transparent hover:bg-primary focus:bg-primary hover:border-primary focus:border-primary']">
       <slot />
     </button>
   </div>
@@ -9,8 +9,9 @@
 <script>
 export default {
   props: {
-    white_text: Boolean
+    flexdisplay: Boolean
   },
+  
   methods: {
     emitclicked(){
       this.$emit("clicked")
--- components/Ui/Buttons/Tertiary.vue
+++ components/Ui/Buttons/Tertiary.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="emitclicked()" class="py-2 px-5 text-accent border-accent focus:text-white hover:text-white border font-medium text-base 2xl:text-lg justify-center duration-500 ease-in-out rounded-md outline-none focus:outline-none" :class="[flexdisplay ? 'flex w-full' : 'block w-fit', standout ? 'bg-primary  border-primary hover:bg-secondary focus:bg-secondary': 'bg-transparent hover:bg-primary focus:bg-primary hover:border-primary focus:border-primary']">
+    <button @click="emitclicked" class="font-normal text-sm xl:text-base 2xl:text-lg hover:text-primary ease-in-out duration-500 border-b border-transparent hover:border-primary select-none text-left outline-none focus:outline-none focus:border-primary" :class="white_text ? 'text-white' : 'text-accent'">
       <slot />
     </button>
   </div>
@@ -9,9 +9,8 @@
 <script>
 export default {
   props: {
-    flexdisplay: Boolean
+    white_text: Boolean
   },
-  
   methods: {
     emitclicked(){
       this.$emit("clicked")
