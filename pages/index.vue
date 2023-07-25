c94fa2634dab3854f165de304bf346d89890fc4c
Qaudri <aquadirmuhammad@gmail.com>
2023-07-25 19:55:31 +0100

    chore: new page design in progress

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <UiTypographyH1>Unleashing Creativity</UiTypographyH1>
+  <div class="h-screen max-w-7xl mx-auto px-6 grid place-items-center">
+    <UiTypographyH1 class="uppercase">Unleashing Creativity</UiTypographyH1>
   </div>
 </template>
