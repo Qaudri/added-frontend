8b1bd351fab20c8ed036ec20e7bd6429aa121e73
Qaudri <aquadirmuhammad@gmail.com>
2023-07-26 14:59:42 +0100

    chore: new font imported and styled

--- app.vue
+++ app.vue
@@ -6,10 +6,15 @@
   </div>
 </template>
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
 html {
   scroll-behavior: smooth;
   -webkit-overflow-scrolling: touch;
+}
+
+.bebas {
+  font-family: Bebas Neue;
 }
 
 body {
