df134bc198cabf940c8ade5c8d196ad7b5ef4c1e
Qaudri <aquadirmuhammad@gmail.com>
2023-07-26 15:41:35 +0100

    chore: new design elements added to header section design

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="ticky top-0 h-screen max-w-7xl mx-auto px-6 grid place-items-center">
+  <header class="ticky top-0 h-fit py-40 md:py-0 md:h-screen max-w-7xl mx-auto px-6 grid place-items-center">
     <div>
       <h1 class="uppercase font-bold text-8xl lg:text-9xl text-center bebas">Ignite  Your<br/>Imagination</h1>
       <UiTypographyH3 class="text-center m-0">Bespoke Creative Services</UiTypographyH3>
@@ -16,5 +16,6 @@
         </UiButtonsTertiary>
       </div>
     </div>
-  </section>
+    <img src="@/assets/images/gradient.png" class="w-72 rotate-45 absolute right-0 -bottom-40" alt="">
+  </header>
 </template>
