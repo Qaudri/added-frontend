41c868dd89d580a425719b426cdec084eeeb6321
Qaudri <aquadirmuhammad@gmail.com>
2023-07-26 15:09:57 +0100

    chore: basic header section design completed

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="h-screen max-w-7xl mx-auto px-6 grid place-items-center">
-    <UiTypographyH1 class="uppercase">Unleashing Creativity</UiTypographyH1>
-  </div>
+  <section class="ticky top-0 h-screen max-w-7xl mx-auto px-6 grid place-items-center">
+    <div>
+      <h1 class="uppercase font-bold text-8xl lg:text-9xl text-center bebas">Ignite  Your<br/>Imagination</h1>
+      <UiTypographyH3 class="text-center m-0">Bespoke Creative Services</UiTypographyH3>
+
+      <div class="flex gap-5 mt-8 justify-center items-center">
+        <UiButtonsPrimary>Discover what we do</UiButtonsPrimary>
+        <UiButtonsTertiary>
+          <div class="flex gap-2 items-center">
+            <span>Scroll Down</span>
+            <div class="animate-bounce">
+              <IconsNext class="w-7 rotate-90 block"></IconsNext>
+            </div>
+          </div>
+        </UiButtonsTertiary>
+      </div>
+    </div>
+  </section>
 </template>
