f8239f1c3bbdd83a88e159b0ef8b6102da7dcb73
waliu <waliuwaheed2021@gmail.com>
2023-07-29 14:14:22 -0500

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,326 @@
 <template>
-  <header class="relative h-fit py-40 md:py-0 md:h-screen grid place-items-center">
-    <div class="max-w-7xl container mx-auto px-6 text-accent">
-      <h1 class="uppercase font-bold text-8xl lg:text-9xl text-center bebas">Ignite  Your<br/>Imagination</h1>
-      <UiTypographyH3 class="text-center m-0">Bespoke Creative Services</UiTypographyH3>
+  <div>
+    <NavigationHeade></NavigationHeade>
+   <div class="hero w-full h-screen bg-orange-500 py-32 sm:py-48">
+     <div class="container mx-auto px-4 sm:px-6 py-16 sm:py-20 lg:py-24" >
+        <div class="w-full  max-w-4xl mx-auto text-center ">
+        <h1 class="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold leading-none py-4 px-6" >We help you accelerate digital transformation</h1>
+        <p class="text-white text-base sm:text-xl leading-6 md:leading-6 my-3 px-6"> With our experienced team, we design and procure tailored solutions for customers with small or medium businesses, big enterprise, or organization </p>
+        </div>
+    </div>
+   </div>
 
-      <div class="flex gap-5 mt-8 justify-center items-center">
-        <UiButtonsPrimary>Discover what we do</UiButtonsPrimary>
-        <UiButtonsTertiary>
-          <div class="flex gap-2 items-center">
-            <span>Scroll Down</span>
-            <div class="animate-bounce">
-              <IconsNext class="w-7 rotate-90 block"></IconsNext>
+     <div class="container mx-auto px-4 sm:px-6 py-16 sm:py-20 lg:py-24">
+        <div class="bg-white shadow-lg rounded-3xl p-6 sm:p-10 -mt-40 relative z-10">
+           <h1 class="text-center text-lg">Clients who trust our expertise</h1>
+           <div>
+             <div class="flex  flex-wrap justify-center gap-4 sm:gap-8 pt-6">
+                <div class="w-24 sm:w-32">
+                <img src="https://res.cloudinary.com/micakindev/image/upload/v1684606162/micakin/web/clients/osun_fzf5uz.jpg" alt="Osun logo" height="100%" width="100%">
+                </div>
+
+             <div class="w-24 sm:w-32">
+                <img src="https://res.cloudinary.com/micakindev/image/upload/v1684605921/micakin/web/clients/tola_d7pwdp.jpg" alt="Tola logo" height="100%" width="100%">
+             </div>
+
+             <div class="w-24 sm:w-32">
+                <img src="https://res.cloudinary.com/micakindev/image/upload/v1684605921/micakin/web/clients/tojum_cne8kx.jpg" alt="Tojum logo" height="100%" width="100%">
+             </div>
+
+             <div class="w-24 sm:w-32">
+                <img src="https://res.cloudinary.com/micakindev/image/upload/v1684605921/micakin/web/clients/greentempo_mrakwh.jpg" alt="GreenTempo logo" height="100%" width="100%">
+             </div>
+
+             <div class="w-24 sm:w-32">
+                <img src="https://res.cloudinary.com/micakindev/image/upload/v1684605921/micakin/web/clients/kanlilly_jql4tj.jpg" alt="Kanlilly logo" height="100%" width="100%">   
+             </div>
+
             </div>
-          </div>
-        </UiButtonsTertiary>
-      </div>
-    </div>
-    <img src="@/assets/images/gradient.png" class="w-72 absolute right-0 -bottom-[15%]" alt="">
-  </header>
-
-  <section class="bg-gray-100 py-20 px-6">
-    <div class="max-w-7xl container mx-auto">
-      <div class="block gap-5 md:flex">
-        <UiTypographyH1 class="bebas text-accent">We Innovate Your Vision.</UiTypographyH1>
-        <UiTypographyH3 class="md:w-1/2 text-accent">We help craft unique and tailored solutions that moves your business forward. Partnering with passionate leaders and visionary entrepreneurs, we apply creativity, strategy, design and data to re-invent businesses, drive growth and orchestrate customer centric transformation.</UiTypographyH3>
-      </div>
-
-      <div class="grid md:grid-cols-3 divide-accent divide-y md:divide-y-0 md:divide-x mt-20 text-accent">
-        <div class="w-full p-5">
-          <div class="w-full lg:w-2/3 mx-auto">
-            <UiTypographyH1 class="bebas inline-flex items-baseline">97
-            <span>
-              <UiTypographyH2 class="bebas">&nbsp;%</UiTypographyH2>
-            </span>
-            </UiTypographyH1>
-            <UiTypographyP>CLIENTS SAW AN INCREASE OF MORE OF 90% ON SALES</UiTypographyP>
-          </div>
-
+           </div>
         </div>
+    </div>
 
-        <div class="w-full p-5">
-          <div class="w-full lg:w-2/3 mx-auto">
-            <UiTypographyH1 class="bebas inline-flex items-baseline">5
-            <span>
-              <UiTypographyH2 class="bebas">&nbsp;M</UiTypographyH2>
-            </span>
-            </UiTypographyH1>
-            <UiTypographyP>PEOPLE REACHED BY OUR CLIENTS WITH US</UiTypographyP>
-          </div>
+    <div class=" container mx-auto px-4 sm:px-6 py-16 sm:py-20 lg:py-24">
+        <div class="w-full ">
+            <h1 class="text-3xl text-center sm:text-4xl text-secondary-900 font-bold"> Core Services</h1>
+            <div  class="grid lg:grid-cols-2 gap-8  mt-10 md:gap-10">
 
+                <div class="bg-white flex flex-col gap-6 md:flex-row lg:flex-col xl:flex-row p-6 sm:p-12 transition h-fit duration-500 rounded-2xl relative shadow-light border border-gray-200">
+                    <img src="https://res.cloudinary.com/micakindev/image/upload/v1684590354/micakin/web/icons/web2_kfsdge.svg" alt="Web Development \ Design icon" class="shrink-0 w-16 h-16 sm:w-28 sm:h-28 flex items-center">
+                    <div class="">
+                        <h1 class="font-bold sm:text-xl">Web Development & Design</h1>
+                        <p class="text-black text-opacity-70 mt-3">We offer top-notch web development services to help your business succeed online. Whether you need a simple brochure website or a complex e-commerce platform, or a data-driven portal development we can help</p>
+                    </div>
+                </div>
+
+                <div class="bg-white flex flex-col gap-6 md:flex-row lg:flex-col xl:flex-row p-6 sm:p-12 transition h-fit duration-500 rounded-2xl relative shadow-light border border-gray-200">
+                    <img src="https://res.cloudinary.com/micakindev/image/upload/v1684590354/micakin/web/icons/ui2_zkjcku.svg" alt="Web Development \ Design icon" class="shrink-0 w-16 h-16 sm:w-28 sm:h-28 flex items-center">
+                    <div class="">
+                        <h1 class="font-bold sm:text-xl">UI/UX Design</h1>
+                        <p class="text-black text-opacity-70 mt-3">You can get an appealing and easy-to-use user interface design for your next project, we are committed to delivering high-quality UI/UX design services that help our clients achieve their business goals.</p>
+                    </div>
+                </div>
+
+              <div class="bg-white flex flex-col gap-6 md:flex-row lg:flex-col xl:flex-row p-6 sm:p-12 transition h-fit duration-500 rounded-2xl relative shadow-light border border-gray-200">
+                    <img src="https://res.cloudinary.com/micakindev/image/upload/v1684590354/micakin/web/icons/presentation2_wutcdb.svg" alt="Presentation Design icon" class="shrink-0 w-16 h-16 sm:w-28 sm:h-28 flex items-center">
+                    <div class="">
+                        <h1 class="font-bold sm:text-xl">Presentation Design</h1>
+                        <p class="text-black text-opacity-70 mt-3">We are a goto for your presentation slides designs and layouts, we understand that presentations are an important part of your brand’s identity. We can help you design your next presentation deck to help you elevate your brand’s presence and make a lasting impact on your audience.</p>
+                    </div>
+                </div>
+
+                <div class="bg-white flex flex-col gap-6 md:flex-row lg:flex-col xl:flex-row p-6 sm:p-12 transition h-fit duration-500 rounded-2xl relative shadow-light border border-gray-200">
+                    <img src="https://res.cloudinary.com/micakindev/image/upload/v1684590351/micakin/web/icons/cloud2_zhveab.svg" alt="Cloud Backup and Maintenance icon" class="shrink-0 w-16 h-16 sm:w-28 sm:h-28 flex items-center">
+                    <div class="">
+                        <h1 class="font-bold sm:text-xl">Cloud Backup and Maintenance</h1>
+                        <p class="text-black text-opacity-70 mt-3">With our cloud backup and maintenance services, you can rest easy knowing that your data is always safe and secure, and your systems are always running at their best. Contact us today to learn more about how we can help protect your agency's data.</p>
+                    </div>
+                </div>
+
+               
+               
+            </div>
         </div>
+    </div>
 
-        <div class="w-full p-5">
-          <div class="w-full lg:w-2/3 mx-auto">
-            <UiTypographyH1 class="bebas inline-flex items-baseline">4.67
-            <span>
-              <UiTypographyH2 class="bebas">&nbsp;M</UiTypographyH2>
-            </span>
-            </UiTypographyH1>
-            <UiTypographyP>HIGH ENGAGEMENT SINCE DAY ONE</UiTypographyP>
-          </div>
+    <div class="container mx-auto px-4 sm:px-6 py-16 sm:py-20 lg:py-24" >
+        <div class="max-w-xs text-left">
+            <h1 class="text-3xl sm:text-4xl mb-9 text-secondary-900 font-bold"> Case Studies </h1>
         </div>
-      </div>
-    </div>
-  </section>
+        <carousel :items-to-show="2.3" :breakpoints="breakpoints"   v-bind="settings" :wrap-around="true" class="flex text-left  ">
+            
+             <slide v-for="slide in tools" :key="slide"   class="">
+              <div class=" relative mx-2 group  transform gap-5 duration-75">
+                <div  class=" md:group-hover:p-4 rounded-lg transition-all duration-500" >
+                <img class="rou rounded-[20px]" :src="slide.img">
+              </div>
+              <div class="bg-gradient-to-t from-black to-transparent absolute bottom-0 w-full pt-40 group-hover:pt-72 pb-6 transition-all duration-300">
+                <div class="p-6 md:p-8 lg:p-16 flex flex-col gap-4">
+                    <h1 class="text-white text-xl text-left md:text-2xl font-bold">{{slide.header}}</h1>
+                    <p class="text-xs sm:text-base hidden group-hover:flex transition-all duration-300 text-white text-left text-opacity-80">{{slide.description}}</p>
 
-  <section class="px-6 py-20 max-w-7xl mx-auto container text-accent">
-    <UiTypographyH1 class="bebas">Services</UiTypographyH1>
-    <div class="grid grid-cols-2 gap-5 lg:gap-16 mt-6">
-    <div class="bg-gray-100 w-full aspect-video"></div>
-    <div class="justify-end flex flex-col">
-      <UiTypographyH1 class="bebas">Branding</UiTypographyH1>
-      <UiTypographyH3 class="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus officiis earum placeat consectetur fuga sed repellendus ea explicabo unde, quas odio. Pariatur facilis nobis distinctio harum explicabo laborum sapiente fugit?</UiTypographyH3>
-      <UiButtonsSecondary class="flex">Learn More <span class="inline"><IconsNext class="w-5"/></span></UiButtonsSecondary>
+                    <ul  class="hidden group-hover:flex transition-all duration-300 flex-wrap gap-2">
+                        <li   class="text-white text-xs px-3 py-1 rounded-full border border-white">HTml5 
+                        </li>
+                        <li   class="text-white text-xs px-3 py-1 rounded-full border border-white">HTml5 
+                        </li>
+                        
+                    </ul>
+                 </div>
+              </div>
+              </div>
+             </slide >
+             <template #addons>
+      
+    </template>
+        </carousel>
+
     </div>
+
+    <div  class="container mx-auto px-4 sm:px-6 py-16 sm:py-20 lg:py-24">
+        <h1 class="text-3xl max-w-md sm:text-4xl text-secondary-900 font-bold"> We have Tailored Solutions for Industries </h1>
+        <div class="grid grid-cols-2 xl:grid-cols-4 mt-8 gap-4 md:gap-10">
+            <div v-for="slide in toolss" :key="slide" class="grayscal relative bg-center bg-cover  hover:bg-accent4-50 border border-accent4-50 hover:border-accent4-100 cursor-pointer transition duration-500 rounded-xl shadow-light" style="background-repeat:no-repeat;;background-image:url(https://res.cloudinary.com/micakindev/image/upload/v1684602220/micakin/web/industries/agriculture_ly3gtk.webp);" >
+                <div class="py-32 sm:py-48"></div>
+                <div class="absolute bottom-0  w-full">
+                    <div class="w-full flex bg-gradient-to-t from-black to-transparent bg-opacity-60 px-3 sm:px-6 rounded-b-xl pt-10 md:pt-32 pb-6 md:pb-8 h-full items- gap-2">
+                     <div class="hidden shrink-0 w-10 h-10 sm:flex items-center justify-center bg-white rounded-full p-2 shadow-light">
+                        <img src="https://res.cloudinary.com/micakindev/image/upload/v1684590351/micakin/web/icons/agriculture_g8iknw.svg" class="text-white" alt="Agricultureicon" height="100%" width="100%">
+                        </div>
+                        <div class="w-full">
+                            <h1 class="text-white text-center sm:text-left font-bold sm:text-xl">Agriculture</h1>
+                            <p class="hidden sm:block text-xs md:text-sm text-white text-opacity-70">We build application and solution to enhance agriculture activities</p>
+                        </div>
+                     </div>
+                  </div>
+            </div>
+        </div>
     </div>
-  </section>
+
+
+   <NavigationFooter></NavigationFooter>
+  </div>
 </template>
+
+<script>
+import pic1 from "../assets/images/pic1.jpg"
+import pic9 from "../assets/images/uoa.jpg"
+import pic2 from "../assets/images/pic2.jpg"
+import pic3 from "../assets/images/pic3.jpg"
+import pic4 from "../assets/images/pic4.jpg"
+import pic5 from "../assets/images/pic5.jpg"
+import pic6 from "../assets/images/pic6.jpg"
+import pic7 from "../assets/images/pic7.jpg"
+import pic8 from "../assets/images/pic8.jpg"
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+export default {
+     
+        data(){
+            return{
+                settings: {
+                itemsToShow: 2.5,
+                snapAlign: 'left',
+            },
+
+            breakpoints:{
+                 500: {
+        itemsToShow: 1.1,
+        snapAlign: 'left',
+      },
+      1024: {
+        itemsToShow: 2.1,
+        snapAlign: 'left',
+      },
+
+    
+            },
+
+                tools: [
+                    {
+                    id : 1,
+                    img:pic5,
+                    header:'Osun State Government Website',
+                    description:'The official website was redesigned from scratch using content management system',
+                    
+                    },
+                    
+                     {
+                    id : 2,
+                    header:'Odori',
+                    description:'A B2C Ordering Platform',
+                    img: pic6,
+                    name:'gucci',
+                    price:'100$',
+                    cnumber:'3',
+                    
+                    },
+
+                     {
+                    id : 3,
+                    header:'Osun State Government Staff Portal',
+                    description:'The official website was redesigned from scratch using content management system',
+                    img: pic7,
+                    name:'gucci',
+                    price:'100$',
+                    cnumber:'3',
+                    
+                    },
+                    
+                     {
+                    id : 4,
+                    header:'Tola Studio Website',
+                    description:'Portfolio website for a brand identity designer',
+                    img: pic8,
+                    name:'gucci',
+                    price:'100$',
+                    cnumber:'3',
+                    
+                    },
+                      {
+                    id : 5,
+                    header:'GTN Website and Data Directory',
+                    description:'Green Tempo Nigeria website design and Lagos green directory portal',
+                    img: pic1,
+                    name:'gucci',
+                    price:'100$',
+                    cnumber:'3',
+                    
+                    },
+
+                    {
+                    id : 6,
+                    header:'Dafotglobal Official Website',
+                    description:'Business website for a consultancy group',
+                    img: pic2,
+                   
+                    
+                    },
+
+                     {
+                    id : 7,
+                    header:'UNDP-OSSH Application Platform',
+                    description:'Portal development for application and screening during 2022 UNDP-OSSH Programme',
+                    img: pic9,
+                    name:'gucci',
+                    price:'100$',
+                    cnumber:'3',
+                    
+                    },
+
+                    {
+                    id : 8,
+                    header:'Tojum',
+                    description:'A landing page web design for a hospitality company in Nigeria',
+                    img: pic3,
+                    name:'gucci',
+                    price:'100$',
+                    cnumber:'3',
+                    
+                    },
+                   
+                ],
+
+              toolss: [
+                    {
+                    id : 1,
+                    img:pic5,
+                    header:'Osun State Government Website',
+                    description:'The official website was redesigned from scratch using content management system',
+                    
+                    },
+                    
+                     {
+                    id : 2,
+                    header:'Odori',
+                    description:'A B2C Ordering Platform',
+                    img: pic6,
+                    name:'gucci',
+                    price:'100$',
+                    cnumber:'3',
+                    
+                    },
+
+                     {
+                    id : 3,
+                    header:'Osun State Government Staff Portal',
+                    description:'The official website was redesigned from scratch using content management system',
+                    img: pic7,
+                    name:'gucci',
+                    price:'100$',
+                    cnumber:'3',
+                    
+                    },
+                    
+                     {
+                    id : 4,
+                    header:'Tola Studio Website',
+                    description:'Portfolio website for a brand identity designer',
+                    img: pic8,
+                    name:'gucci',
+                    price:'100$',
+                    cnumber:'3',
+                    
+                    },
+    
+                   
+                ],
+            }
+        },
+
+
+
+components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+    }
+
+
+</script>
+
+<style>
+
+</style>
