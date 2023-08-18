050fda78b2b225779c5e887b902709634142273a
Qaudri <aquadirmuhammad@gmail.com>
2023-08-18 16:26:15 +0100

    refactor footer design updated

--- components/Navigation/Footer.vue
+++ components/Navigation/Footer.vue
@@ -1,7 +1,10 @@
 <template>
-  <footer class="max-w-7xl px-6 py-20 mx-auto">
-    <div class="w-36" >
-      <NavigationLogo />
+  <footer class="bg-accent border-t border-white text-white">
+    <div class="max-w-7xl container mx-auto px-6 pt-20 pb-8 flex justify-between items-center">
+      <div class="w-36" >
+        <NavigationLogo />
+      </div>
+      <UiTypographyP>&copy; Added Digital Solutions, 2023</UiTypographyP>
     </div>
   </footer>
 </template>
