cc3f7a524dc827473586be5ace6ee5f1a66431c4
Qaudri <aquadirmuhammad@gmail.com>
2023-10-09 18:01:20 +0100

    footer updated for responsiveness

--- components/Navigation/Footer.vue
+++ components/Navigation/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="text-accent">
-    <div class="max-w-7xl container mx-auto px-6 p-8 lg:flex justify-between items-center">
+    <div class="max-w-7xl container mx-auto px-6 p-8 md:flex justify-between items-center">
       <div class="" >
         <div class="flex gap-4 items-center">
         <NuxtLink class="border-[3px] border-accent rounded-md w-10 h-10 grid place-items-center">
@@ -18,7 +18,7 @@
         </div>
         <UiTypographyP class="mt-4">&copy; Added Digital Solutions, 2023</UiTypographyP>
       </div>
-      <div class="grid gap-2 text-left lg:text-right mt-8">
+      <div class="grid gap-2 text-left md:text-right mt-8">
       <h2 class="text-primary text-2xl font-bold">Added Digital Solutions</h2>
       <UiTypographyP class="text-accent capitalize">1st floor, ID Global building, beside Allan pharmacy,<br/>opp Iwo city hall, Iwo, Osun state, nigeria</UiTypographyP>
       <h2 class="text-primary text-2xl font-bold">+2348066600820</h2>
