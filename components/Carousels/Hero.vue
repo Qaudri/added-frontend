36163708f9a13c77a4cd47586d17a71860006105
Qaudri <aquadirmuhammad@gmail.com>
2023-10-10 16:41:37 +0100

    responsive updates made to hero component

--- components/Carousels/Hero.vue
+++ components/Carousels/Hero.vue
@@ -3,15 +3,15 @@
       <div class="glide__track" data-glide-el="track">
         <div class="glide__slides">
           <div class="glide__slide">
-            <div class="flex min-h-screen md:h-screen md:items-center lg:items-end max-w-7xl container mx-auto px-6">
+            <div class="flex h-72 md:h-screen md:items-center lg:items-end max-w-7xl container mx-auto px-6">
               <div class="md:flex justify-between items-center pt-20 md:pt-0">
                 <Typewriter class="text-accent  md:w-1/2 w-full" />
-                <img src="@/assets/images/hero.svg" alt="" class="md:w-1/2">
+                <img src="@/assets/images/hero.svg" alt="" class="hidden md:block w-1/2">
               </div>
             </div>
           </div>
           <div v-for="portfolio in portfolios" :key="portfolio.id" class="glide__slide">
-            <div :style="{ background:`url(${portfolio.image})`}" class="background h-screen w-full flex justify-end items-end">
+            <div :style="{ background:`url(${portfolio.image})`}" class="background h-72 md:h-screen w-full flex justify-end items-end">
             </div>
           </div>
 
