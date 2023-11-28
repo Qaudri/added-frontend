d66e63e51796ea2b222addcb84d7c5e3821a38e5
Quadir <aquadirmuhammad@gmail.com>
2023-11-28 14:10:13 +0100

    navbar hyperlinks updated

--- components/Navigation/NavBar.vue
+++ components/Navigation/NavBar.vue
@@ -9,13 +9,13 @@
       <nav :class="menu_revealed ? 'translate-x-0' : '-translate-x-full'" class="md:h-fit z-100 md:static md:bg-transparent w-full md:w-fit fixed h-screen left-0 top-[72px] md:top duration-150 ease-in-out md:translate-x-0 bg-white">
         <ul class="grid grid-cols-1 place-items-center md:flex gap-8 md:gap-3 lg:gap-5 items-center px-6 pt-20 md:p-0">
           <li>
-            <UiButtonsTertiary>home</UiButtonsTertiary>
+            <UiButtonsTertiary @clicked="$router.push({path: '/'})">home</UiButtonsTertiary>
           </li>
           <li>
             <UiButtonsTertiary>discipline +</UiButtonsTertiary>
           </li>
           <li>
-            <UiButtonsTertiary @clicked="$router.push('https://academy.addeddigital.com/')">academy</UiButtonsTertiary>
+            <NuxtLink class="font-semibold text-sm xl:text-base 2xl:text-lg hover:text-primary ease-in-out duration-300  hover:border-primary select-none text-left outline-none" to="https://academy.addeddigital.com/">academy</NuxtLink>
           </li>
           <li>
             <UiButtonsTertiary>about us</UiButtonsTertiary>
