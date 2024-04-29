e2d3eac4e48bff84aa38b024ef3a699ecbe8e804
Quadir <aquadirmuhammad@gmail.com>
2024-04-29 20:03:00 +0100

    hero section redesigned

--- components/Carousels/Hero.vue
+++ components/Carousels/Hero.vue
@@ -1,20 +1,18 @@
 <template>
-  <header class="hero">
-      <div class="glide__track" data-glide-el="track">
-        <ul class="glide__slides">
-          <li v-for="portfolio in portfolios" :key="portfolio.id" class="glide__slide">
-            <div :style="{ background:`url(${portfolio.image})`}" class="background h-screen-50 md:h-screen-60 lg:h-screen w-full flex justify-center items-center">
-              <div class="bg-black bg-opacity-70 p-10 text-white max-w-3xl">
-                <UiTypographyH2 class="uppercase">ultimate solutions for your digital needs<span class="text-primary">.</span></UiTypographyH2>
-                <UiTypographyP class="mt-6">We also provide a wide range of digital marketing and content creation services to help your business reach more customers. Contact us today for a free consultation.</UiTypographyP>
-
-                <UiButtonsTalk class="text-primary w-32 mt-8 cursor-pointer hover:text-secondary duration-300 ease-in-out" />
-              </div>
-            </div>
-          </li>
-
-        </ul>
-      </div>
+  <header class="hero relative">
+    <div class="bg-black bg-opacity-70 p-10 text-white max-w-2xl absolute z-10 right-0 bottom-0">
+      <UiTypographyH2 class="uppercase">ultimate solutions for your digital needs<span class="text-primary">.</span></UiTypographyH2>
+      <UiTypographyP class="mt-6">We also provide a wide range of digital marketing and content creation services to help your business reach more customers. Contact us today for a free consultation.</UiTypographyP>
+      <UiButtonsTalk class="text-primary w-32 mt-8 cursor-pointer hover:text-secondary duration-300 ease-in-out" />
+    </div>
+    <div class="glide__track" data-glide-el="track">
+      <ul class="glide__slides">
+        <li v-for="portfolio in portfolios" :key="portfolio.id" class="glide__slide">
+          <div :style="{ background:`url(${portfolio.image})`}" class="background h-screen-50 md:h-screen-60 lg:h-screen w-full">
+          </div>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
 <script setup>
